368b30a8fa109698d5cdf3f84b196859d2a043a7
jjjvvvvv <jonathanvrg554@gmail.com>
2020-08-31 21:53:43 -0400

    Location stuff (sort of) worked out. About to start a challenge to refactor all location stuff into new file.
    Getting error from Google play store. Stack Overflow thinks it's a mismatched dependency.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clima/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -7,23 +8,28 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // This is only seems to work upon terminating. I don't know why. "stateful widget lifecycle" video challenge
+    getLocation();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
+      body: Text(
+        'CLIMA',
       ),
     );
   }
