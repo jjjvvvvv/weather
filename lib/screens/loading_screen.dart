7a0009672fad301d2b932814a18934f14ec15a15
jjjvvvvv <jonathanvrg554@gmail.com>
2020-09-03 21:43:07 -0400

    added imperial units.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -28,7 +28,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     longitude = location.longitude;
 
     NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=imperial');
 
     var weatherData = await networkHelper.getData();
 
