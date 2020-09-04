7d31ba3e379edf1a994f3f9f51499b618db5446d
jjjvvvvv <jonathanvrg554@gmail.com>
2020-09-03 21:39:29 -0400

    added loading spinner.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
-//import 'package:clima/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'e4257fca279753a87814c1bdf16d73e5';
 
@@ -30,10 +31,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingCircle(
+          color: Colors.grey,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
